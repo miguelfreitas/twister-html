5c35807932949045bca7f6e7a489c4ed9d751baa
Simon Grim <scary@tasty.sexy>
2015-05-26 01:47:53 +0500

    add missing flags to clone event too from DM template textareas, code restyling

--- js/twister_directmsg.js
+++ js/twister_directmsg.js
@@ -5,41 +5,37 @@
 
 function requestDMsnippetList(dmThreadList) {
     var followList = [];
-    for( var i = 0; i < followingUsers.length; i++ ) {
+    for (var i = 0; i < followingUsers.length; i++)
         followList.push({username:followingUsers[i]});
-    }
-    for( var i = 0; i < groupChatAliases.length; i++ ) {
+    for (var i = 0; i < groupChatAliases.length; i++)
         followList.push({username:groupChatAliases[i]});
-    }
 
-    twisterRpc("getdirectmsgs", [defaultScreenName, 1, followList],
-           function(req, ret) {processDMsnippet(ret, dmThreadList);}, dmThreadList,
-           function(req, ret) {console.log("ajax error:" + ret);}, null);
+    twisterRpc('getdirectmsgs', [defaultScreenName, 1, followList],
+        function(req, ret) {processDMsnippet(ret, dmThreadList);}, dmThreadList,
+        function(req, ret) {console.log('ajax error:' + ret);}, null
+    );
 }
 
 function processDMsnippet(dmUsers, dmThreadList) {
     dmThreadList.empty();
 
-    for( var u in dmUsers ) {
-        if( dmUsers.hasOwnProperty(u) ) {
+    for (var u in dmUsers) {
+        if (dmUsers[u]) {
             // convert snipped to html and add it to date-sorted list
             var dmItem = dmDataToSnippetItem(dmUsers[u][0], u);
-            var timeDmItem = parseInt(dmItem.attr("data-time"));
+            var timeDmItem = parseInt(dmItem.attr('data-time'));
             var existingItems = dmThreadList.children();
-            var j = 0;
-            for( j = 0; j < existingItems.length; j++) {
+            for (var j = 0; j < existingItems.length; j++) {
                 var streamItem = existingItems.eq(j);
-                var timeExisting = streamItem.attr("data-time");
-                if( timeExisting == undefined ||
-                    timeDmItem > parseInt(timeExisting) ) {
+                var timeExisting = streamItem.attr('data-time');
+                if (typeof timeExisting === 'undefined' || timeDmItem > parseInt(timeExisting)) {
                     // this post in stream is older, so post must be inserted above
                     streamItem.before(dmItem);
                     break;
                 }
             }
-            if( j == existingItems.length ) {
+            if (j === existingItems.length)
                 dmThreadList.append(dmItem);
-            }
         }
     }
     $.MAL.dmThreadListLoaded();
@@ -52,84 +48,86 @@ function requestDmConversationModal(postboard, dm_screenname) {
     }
 }
 
-function requestDmConversation(dmConvo,dm_screenname) {
+function requestDmConversation(postboard, dm_screenname) {
     var since_id = undefined;
 
-    var oldItems = dmConvo.children();
-    if( oldItems.length ) {
-        since_id = parseInt(oldItems.eq(oldItems.length-1).attr("data-id"));
-    }
+    var oldItems = postboard.children();
+    if (oldItems.length)
+        since_id = parseInt(oldItems.eq(oldItems.length - 1).attr('data-id'));
 
-    var userDmReq = [{username:dm_screenname}];
-    if( since_id != undefined ) userDmReq[0].since_id = since_id;
+    var userDmReq = [{username: dm_screenname}];
+    if (typeof since_id !== 'undefined')
+        userDmReq[0].since_id = since_id;
+
     var count = 100;
-    twisterRpc("getdirectmsgs", [defaultScreenName,count,userDmReq],
-               function(args, ret) { processDmConversation(args.dmConvo, args.dmUser, ret); },
-                                   {dmConvo:dmConvo,dmUser:dm_screenname},
-               function(arg, ret) { var msg = ("message" in ret) ? ret.message : ret;
-                                    alert(polyglot.t("ajax_error", { error: msg })); }, null);
+    twisterRpc('getdirectmsgs', [defaultScreenName, count, userDmReq],
+        function(args, ret) {processDmConversation(args.postboard, args.dm_screenname, ret);},
+            {postboard: postboard, dm_screenname: dm_screenname},
+        function(arg, ret) {
+            var msg = (ret.message) ? ret.message : ret;
+            alert(polyglot.t('ajax_error', {error: msg}));
+        }, null
+    );
 }
 
-function processDmConversation(dmConvo, dm_screenname, dmData) {
+function processDmConversation(postboard, dm_screenname, dmData) {
     var lastId = undefined;
-    if(dm_screenname in dmData) {
+
+    if (dmData[dm_screenname]) {
         var dmList = dmData[dm_screenname];
-        if( dmList.length ) {
-            for( var i = 0; i < dmList.length; i++) {;
-                var dmItem = dmDataToConversationItem(dmList[i],defaultScreenName,dm_screenname);
-                dmItem.attr("data-id",dmList[i].id);
-                dmConvo.append(dmItem);
+        if (dmList.length) {
+            for (var i = 0; i < dmList.length; i++) {
+                var dmItem = dmDataToConversationItem(dmList[i], defaultScreenName, dm_screenname)
+                    .attr('data-id', dmList[i].id)
+                    .appendTo(postboard)
+                ;
                 lastId = dmList[i].id;
             }
-            $.MAL.dmChatListLoaded(dmConvo);
+            $.MAL.dmChatListLoaded(postboard);
         }
     }
-    if( lastId != undefined ) {
+    if (typeof lastId !== 'undefined')
         resetNewDMsCountForUser(dm_screenname, lastId);
-    }
 }
 
-function directMsgSubmit(e)
-{
+function directMsgSubmit(e) {
     e.stopPropagation();
     e.preventDefault();
-    var $this = $( this );
-    var $replyText = $this.closest(".post-area-new").find("textarea");
 
-    var $dmConversation = $(".directMessages");
+    var replyText = $(this).closest('.post-area-new textarea');
 
-    newDirectMsg($replyText.val(), $dmConversation.attr("data-dm-screen-name"));
+    newDirectMsg(replyText.val(), $('.directMessages').attr('data-dm-screen-name'));
 
-    $replyText.val("");
+    replyText.val('');
 }
 
 function newDirectMsg(msg,  dm_screenname) {
-    if( lastPostId != undefined ) {
-        var paramsOrig = [defaultScreenName, lastPostId + 1, dm_screenname, msg]
-        var paramsOpt = paramsOrig
-        var copySelf = ($.Options.dmCopySelf.val === 'enable')
-        if( copySelf && dm_screenname[0] !== '*' ) {
+    if (typeof lastPostId !== 'undefined') {
+        var paramsOrig = [defaultScreenName, lastPostId + 1, dm_screenname, msg];
+        var paramsOpt = paramsOrig;
+        var copySelf = $.Options.dmCopySelf.val === 'enable';
+        if (copySelf && dm_screenname[0] !== '*')
             paramsOpt = paramsOrig.concat(true)
-        }
 
-        twisterRpc("newdirectmsg", paramsOpt,
-                   function(arg, ret) {
-                        incLastPostId();
-                        if( arg.copySelf ) incLastPostId();
-                    }, {copySelf:copySelf},
-                   function(arg, ret) {
-                        // fallback for older twisterd (error: no copy_self parameter)
-                        twisterRpc("newdirectmsg", arg.paramsOrig,
-                                   function(arg, ret) { incLastPostId(); }, null,
-                                   function(arg, ret) {
-                                       var msg = ("message" in ret) ? ret.message : ret;
-                                       alert("Ajax error: " + msg);
-                                   }, null);
-                    }, {paramsOrig:paramsOrig}
+        twisterRpc('newdirectmsg', paramsOpt,
+            function(arg, ret) {
+                incLastPostId();
+                if (arg.copySelf)
+                    incLastPostId();
+            }, {copySelf: copySelf},
+            function(arg, ret) {
+                // fallback for older twisterd (error: no copy_self parameter)
+                twisterRpc('newdirectmsg', arg.paramsOrig,
+                    function(arg, ret) {incLastPostId();}, null,
+                    function(arg, ret) {
+                        var msg = (ret.message) ? ret.message : ret;
+                        alert('Ajax error: ' + msg);
+                    }, null
+                );
+            }, {paramsOrig: paramsOrig}
         );
-    } else {
-        alert(polyglot.t("Internal error: lastPostId unknown (following yourself may fix!)"));
-    }
+    } else
+        alert(polyglot.t('Internal error: lastPostId unknown (following yourself may fix!)'));
 }
 
 // dispara o modal de direct messages
@@ -141,7 +139,7 @@ function directMessagesPopup() {
 
     modal = openModal({
         classAdd: 'directMessages',
-        content: $('.direct-messages-template').children().clone(),
+        content: $('.direct-messages-template').children().clone(true),
         title: polyglot.t('Direct Messages')
     });
 
@@ -151,8 +149,8 @@ function directMessagesPopup() {
         .css('display', 'inline')
         .attr('title', polyglot.t('Mark all as read'))
         .on('click', function() {
-            for (var k in _newDMsPerUser) {
-                _newDMsPerUser[k] = 0;
+            for (var user in _newDMsPerUser) {
+                _newDMsPerUser[user] = 0;
             }
             saveDMsToStorage();
             $.MAL.updateNewDMsUI(getNewDMsCount());
@@ -168,7 +166,7 @@ function openDmWithUserModal(dm_screenname) {
 
     modal = openModal({
         classAdd: 'directMessages',
-        content: $('.messages-thread-template').children().clone(),
+        content: $('.messages-thread-template').children().clone(true),
         title: polyglot.t('direct_messages_with', {username: '<span>' + dm_screenname + '</span>'})
     });
 
@@ -181,24 +179,22 @@ function openDmWithUserModal(dm_screenname) {
 
     requestDmConversationModal(modal.self.find('.direct-messages-thread').empty(), dm_screenname);
 
-    $('.dm-form-template').children().clone()
+    $('.dm-form-template').children().clone(true)
         .addClass('open').appendTo(modal.content).fadeIn('fast');
 }
 
-
 function initInterfaceDirectMsg() {
+    $('.direct-messages').attr('href','#directmessages');
+    $('.userMenu-messages a').attr('href','#directmessages');
 
-    $( ".direct-messages" ).attr("href","#directmessages");
-    $( ".userMenu-messages a" ).attr("href","#directmessages");
+    $('#dm-snippet-template').on('click', function() {
+        window.location.hash = '#directmessages?user=' + $(this).attr('data-dm-screen-name');
+    });
 
-    $( "#dm-snippet-template" ).bind( "click", function(){
-        window.location.hash='#directmessages?user='+$(this).attr("data-dm-screen-name");
-    } );
-
-    $( ".dm-submit").click( directMsgSubmit );
-    $( ".direct-messages-with-user" ).bind( "click", function() {
-        window.location.hash='#directmessages?user='+$(this).closest("[data-screen-name]").attr("data-screen-name");
-    } );
-
+    $('.dm-submit').on('click', directMsgSubmit);
+    $('.direct-messages-with-user').on('click', function() {
+        window.location.hash = '#directmessages?user=' +
+            $(this).closest('[data-screen-name]').attr('data-screen-name');
+    });
 }
 
