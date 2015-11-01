614d96b470f2ccccb66ca61843cbf4fa8d8861f7
Simon Grim <scary@tasty.sexy>
2015-11-02 02:30:01 +0500

    delete missed console.log

--- js/twister_directmsg.js
+++ js/twister_directmsg.js
@@ -28,7 +28,6 @@ function processDMsnippet(dmUsers, dmThreadList, forGroup) {
 
         // convert snipped to html and add it to date-sorted list
         var dmItem = dmDataToSnippetItem(dmUsers[u][0], u);
-        console.log(u + ': ' + _newDMsPerUser[u]);
         if (_newDMsPerUser[u] > 0) {
             dmItem.addClass('new')
                 .find('.messages-qtd').text(_newDMsPerUser[u]).show();
